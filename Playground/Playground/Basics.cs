f56bb0fce6b1b852d61089fa951f6b67966f579b
André Davis <CubicleJockey@users.noreply.github.com>
2017-08-12 21:02:18 -0700

    Basic Multip Theory

--- Playground/Playground/Basics.cs
+++ Playground/Playground/Basics.cs
@@ -8,9 +8,10 @@ namespace Playground
         [Fact]
         public void BasicFact()
         {
+            //Method to test
             bool IsOdd(int number)
             {
-                return number % 2 != 0;
+                return number % 2 == 1;
             }
 
             //Odd Checks
@@ -28,6 +29,39 @@ namespace Playground
             Assert.False(IsOdd(8));
             Assert.False(IsOdd(10));
             Assert.False(IsOdd(12));
+        }
+
+        [Theory]
+        [InlineData(2)]
+        [InlineData(4)]
+        [InlineData(6)]
+        [InlineData(8)]
+        [InlineData(12)]
+        [InlineData(14)]
+        public void BasicTheory(int value)
+        {
+            bool IsEven(int number)
+            {
+                return number % 2 == 0;
+            }
+            
+            Assert.True(IsEven(value));
+        }
+
+        [Theory]
+        [InlineData(1, 3)]
+        [InlineData(2, 2)]
+        [InlineData(3, 1)]
+        [InlineData(9, -5)]
+        [InlineData(10, -6)]
+        public void MultiValueTheory(int lhs, int rhs)
+        {
+            int Add(int l, int r)
+            {
+                return l + r;
+            }
+
+            Assert.Equal(4, Add(lhs, rhs));
         }
     }
 }
